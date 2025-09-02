b9f437d5a075b0086fdb0a94a7ed5d1a101b0740
mr-mr7 <mohamadrezanaderi39@gmail.com>
2025-09-02 08:45:59 +0330

    fix: Checking hasMoadianCredentials in SetMoadianCredentialsMiddleware

--- src/Middleware/SetMoadianCredentialsMiddleware.php
+++ src/Middleware/SetMoadianCredentialsMiddleware.php
@@ -4,11 +4,12 @@ namespace Jooyeshgar\Moadian\Middleware;
 
 use Closure;
 use Jooyeshgar\Moadian\Facades\Moadian;
+
 class SetMoadianCredentialsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        if (auth()->check()) {
+        if (auth()->check() && auth()->user()->hasMoadianCredentials()) {
             Moadian::forUser(auth()->id());
         }
 
